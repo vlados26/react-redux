314a9bd8d2fdc5a8932f87e19c7d8a205666f16e
Vlad Sirotkin <sirotkin96@gmail.com>
2019-01-09 09:05:23 +0300

    all changes

---
+++ src/components/projects/ProjectSummary.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const ProjectSummary = ({project}) => {
+  console.log(project);
+  return (
+    <div className="card z-depth-0 project-summary">
+      <div className="card-content grey-text text-darken-3">
+        <span className="card-title">{project.title}</span>
+        <p>{project.title}</p>
+        <p className="grey-text">{project.content}</p>
+      </div>
+    </div>
+  )
+}
+
+export default ProjectSummary;
